3cc5939d8565bbf209df4ba4ebdfd501b15d91ec
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-12-05 12:10:18 +0600

    feat: update hero zindex stacking

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ const Hero = () => {
   return (
     <section className="w-full overflow-hidden">
       <Container className="py-40 relative">
-        <h1 className="w-full max-w-3xl mx-auto text-3xl md:text-4xl text-center font-semibold transition-transform">
+        <h1 className="w-full max-w-3xl mx-auto text-3xl md:text-4xl text-center font-bold transition-transform z-20">
           NoorStyle – Islamic Fashion E-commerce Template
         </h1>
 
-        <div className="absolute -top-4 -right-4 h-80 w-56 lg:h-96  rotate-45 rounded-xl bg-gradient-to-tr from-yellow-500 to-orange-800 filter blur-3xl opacity-20"></div>
-        <div className="absolute -bottom-4 -left-4 h-80 w-56 lg:h-96  rotate-45 rounded-xl bg-gradient-to-r from-orange-500 to-yellow-800 filter blur-3xl opacity-20"></div>
+        <div className="-z-10 absolute -top-4 -right-4 h-80 w-56 lg:h-96  rotate-45 rounded-xl bg-gradient-to-tr from-yellow-500 to-orange-800 filter blur-3xl opacity-20"></div>
+        <div className="-z-10 absolute -bottom-4 -left-4 h-80 w-56 lg:h-96  rotate-45 rounded-xl bg-gradient-to-r from-orange-500 to-yellow-800 filter blur-3xl opacity-20"></div>
       </Container>
     </section>
   );
