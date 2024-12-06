819845bd3b716999585194519ba4d9d2af1f2047
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-12-06 11:51:30 +0600

    feat: add react router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import Container from "./Container";
 import Button from "./Button";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 h-14 w-full bg-white border-b border-b-gray-200 flex items-center z-50">
       <Container className="w-full max-w-screen-lg mx-auto px-4 flex items-center justify-between gap-4">
-        <img
-          src="/favicon.png"
-          alt="logo"
-          className="size-8 object-cover shrink-0"
-        />
+        <Link to="/">
+          <img
+            src="/favicon.png"
+            alt="logo"
+            className="size-8 object-cover shrink-0"
+          />
+        </Link>
         <Button>
-          <a href="/" target="_blank">Purchase Now</a>
+          <Link href="/" target="_blank">
+            Purchase Now
+          </Link>
         </Button>
       </Container>
     </nav>
