9739d6d7d796d740d133fc60fafacaa09dc6bed7
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-12-06 15:23:14 +0600

    feat: make demo pages data

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+export const DEMO = {
+  home_pages: [
+    {
+      title: "Home",
+      path: "https://noorstyle.vercel.app",
+      imgSrc: "/images/home-1.png",
+    },
+    {
+      title: "Home Video",
+      path: "https://noorstyle.vercel.app/home-3",
+      imgSrc: "/images/home-2.png",
+    },
+    {
+      title: "Home Slider",
+      path: "https://noorstyle.vercel.app/home-2",
+      imgSrc: "/images/home-3.png",
+    },
+  ],
+  auth_pages: [
+    {
+      title: "Login",
+      path: "https://noorstyle.vercel.app/login",
+      imgSrc: "/images/login.png",
+    },
+    {
+      title: "Register",
+      path: "https://noorstyle.vercel.app/register",
+      imgSrc: "/images/register.png",
+    },
+    {
+      title: "Reset Password",
+      path: "https://noorstyle.vercel.app/reset-password",
+      imgSrc: "/images/reset-password.png",
+    },
+  ],
+  other_pages: [
+    {
+      title: "Shop",
+      path: "https://noorstyle.vercel.app/shop",
+      imgSrc: "/images/shop.png",
+    },
+    {
+      title: "Product Details",
+      path: "https://noorstyle.vercel.app/shop/1",
+      imgSrc: "/images/product-details.png",
+    },
+    {
+      title: "Product Quick View",
+      path: "https://noorstyle.vercel.app/shop/1",
+      imgSrc: "/images/quick-view.png",
+    },
+    {
+      title: "Wishlist",
+      path: "https://noorstyle.vercel.app/wishlist",
+      imgSrc: "/images/wishlist.png",
+    },
+    {
+      title: "Cart",
+      path: "https://noorstyle.vercel.app/cart",
+      imgSrc: "/images/cart.png",
+    },
+    {
+      title: "Checkout",
+      path: "https://noorstyle.vercel.app/checkout",
+      imgSrc: "/images/checkout.png",
+    },
+    {
+      title: "Blog",
+      path: "https://noorstyle.vercel.app/blog",
+      imgSrc: "/images/blog.png",
+    },
+    {
+      title: "Blog Details",
+      path: "https://noorstyle.vercel.app/blog/top-trends-in-islamic-fashion-for-this-season",
+      imgSrc: "/images/blog-details.png",
+    },
+    {
+      title: "Contact",
+      path: "https://noorstyle.vercel.app/contact",
+      imgSrc: "/images/contact.png",
+    },
+  ],
+};
