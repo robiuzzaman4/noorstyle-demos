acc036cf691247407e5ef6a4226ed90881ce1351
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-12-06 17:49:01 +0600

    feat: update badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,13 +2,15 @@ import { cn } from "../utils/cn";
 
 const Badge = ({ children, className }) => {
   return (
-    <div
-      className={cn(
-        "px-3 py-1 rounded-full bg-white border border-orange-500 shadow text-sm font-medium",
-        className
-      )}
-    >
-      {children}
+    <div className={cn("rounded-full bg-orange-50 p-1.5")}>
+      <div
+        className={cn(
+          "px-3 py-1 w-full h-full bg-white shadow border border-orange-500 text-sm font-medium rounded-full",
+          className
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 };
