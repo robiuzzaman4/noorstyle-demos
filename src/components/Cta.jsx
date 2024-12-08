3c7e61eb4621e7ce3a6177ff805504e5a04145ee
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-12-08 12:39:59 +0600

    feat: cta section done

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -4,32 +4,35 @@ import Container from "./Container";
 const Cta = () => {
   return (
     <section className="py-20 px-4">
-      <Container className="bg-zinc-900 py-20 rounded-xl px-6">
-        <div className="w-full max-w-4xl mx-auto grid md:grid-cols-2 gap-12">
+      <Container className="bg-zinc-900 py-20 rounded-xl relative overflow-hidden">
+        <div className="w-full max-w-4xl mx-auto">
           <span className="flex flex-col gap-6">
-            <p className="text-lg font-bold text-center md:text-start text-zinc-300 uppercase">
+            <p className="text-lg font-bold text-center text-zinc-300 uppercase z-40">
               Get Started with{" "}
               <span className="text-orange-500">NoorStyle</span>
             </p>
-            <h1 className="text-3xl md:text-4xl lg:text-5xl text-center md:text-start font-semibold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-violet-500">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl text-center font-semibold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-violet-500 z-40">
               Run Your Business Immediately.
             </h1>
-            <button className="group relative inline-flex transform items-center justify-center whitespace-nowrap text-base font-medium transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 disabled:pointer-events-none disabled:opacity-50 h-12 rounded-full bg-orange-500 text-white shadow hover:ring-2 hover:ring-orange-500 ring-offset-zinc-950 hover:ring-offset-2 focus:ring-offset-2 overflow-hidden">
+            <button className="w-full sm:max-w-60 mx-auto group relative inline-flex transform items-center justify-center whitespace-nowrap text-base font-medium transition-all duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 disabled:pointer-events-none disabled:opacity-50 h-12 rounded-full bg-orange-500 text-white shadow hover:ring-2 hover:ring-orange-500 ring-offset-zinc-900 hover:ring-offset-2 focus:ring-offset-2 overflow-hidden z-40">
               <Link
                 to="https://themeforest.net/user/wenextcoder"
                 target="_blank"
                 className="h-full w-full flex items-center justify-center px-8 gap-1"
               >
-                <p>Try for free</p>
+                <p>Purchase Now</p>
                 <ArrowRight
                   size={16}
                   className="group-hover:transform group-hover:translate-x-1 transition-all duration-300"
                 />
               </Link>
-              <div className="ease-in-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white/50 opacity-25 transition-all duration-500 group-hover:left-[120%]"></div>
+              <div className="ease-in-[cubic-bezier(0.19,1,0.22,1)] absolute -left-[75px] -top-[50px] -z-10 h-[155px] w-8 rotate-[35deg] bg-white opacity-25 transition-all duration-500 group-hover:left-[120%]"></div>
             </button>
           </span>
         </div>
+
+        <div className="absolute -top-4 -right-4 h-80 w-56 lg:h-96  rotate-45 rounded-xl bg-gradient-to-tr from-yellow-500 to-rose-500 filter blur-3xl opacity-20"></div>
+        <div className="absolute -bottom-4 -left-4 h-80 w-56 lg:h-96  rotate-45 rounded-xl bg-gradient-to-r from-violet-500 to-sky-500 filter blur-3xl opacity-20"></div>
       </Container>
     </section>
   );
